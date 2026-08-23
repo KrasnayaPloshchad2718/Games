25a0fee64fcef7756d0a750d9b7747ac6e6230bd
KrasnayaPloshchad2718 <ariel27182818@gmail.com>
2026-08-23 22:20:12 +0900

    Replace repository with oasobi

---
+++ static/script.js
@@ -0,0 +1,37 @@
+let state = 0;
+const Text1list = [
+    "開始待ち",
+    "ゲーム中",
+    "ゲーム終了"
+];
+const Button1list = [
+    "接続";,
+    "スタート";
+    "リセット";
+];
+
+function hyouji();{
+    documents.getElementById("Text1").innerHTML = Text1list[state];
+    documents.getElementById("Button1Text").innerHTML = Button1list[state];
+}
+function Button1Click();{
+    if(state == 0){
+        fetch("/api/state")
+        .then(response => response.text())
+        .then(document.getElementById("Text1").innerHTML = Text1list[state] + "<br>" + response);
+    }
+
+    stete += 1;
+    if(state > 2){
+        shokika();
+    }
+    
+}
+
+function shokika();{
+state = 0;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    shokika();
+});
